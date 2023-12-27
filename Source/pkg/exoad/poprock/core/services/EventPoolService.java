0b1bdab980e1ad3f55e921299f1b32abba9f0ff6
exoad <jackmeng0814@gmail.com>
2023-12-26 21:43:18 -0500

    `fix` new documentation for new expanded files

--- Source/pkg/exoad/poprock/core/services/EventPoolService.java
+++ Source/pkg/exoad/poprock/core/services/EventPoolService.java
@@ -1,7 +1,5 @@
 package pkg.exoad.poprock.core.services;
 
-import java.util.*;
-
 import net.exoad.annotations.NotVirtual;
 import net.exoad.annotations.ServiceClass;
 import net.exoad.annotations.VolatileImpl;
@@ -9,81 +7,84 @@ import pkg.exoad.poprock.core.debug.DebugService;
 import pkg.exoad.poprock.core.debug.DebuggableMixin;
 import pkg.exoad.poprock.core.struct.Pair;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Optional;
+
 /**
  * <h2>EventPool</h2> - <strong>The main global event registry for Poprock
- * both the library and app for dispatching events.</strong> It is structured
- * using a Map making it so that listeners can subscribe to a specific payload
- * type (a class/record that implements
- * {@link EventPoolService.EventPayload}).
+ * both the library and app for dispatching events.</strong> It is structured using a Map
+ * making it so that listeners can subscribe to a specific payload type (a class/record
+ * that implements {@link EventPoolService.EventPayload}).
  * <p>
  * <h3>Events can be dispatched by anyone</h3>, which is a downside to
- * this method, but it proves robust for the most part in terms of ease of use.
- * Especially because there are no special permissions and other shenanigans
- * that goes into just dispatching a simple event.
+ * this method, but it proves robust for the most part in terms of ease of use. Especially
+ * because there are no special permissions and other shenanigans that goes into just
+ * dispatching a simple event.
+ * </p>
  * <p>
  * <h3>Listeners must know their EventPool</h3>, this is the only
- * measure that goes into categorizing data passing through. Listeners must know
- * the pool id {@link #registerEventPool(int)} in order to subscribe to it.
- * <p>
- * <h3>Event Pools are ephemeral</h3>. If you are using Poprock as
- * a library and never call
- * {@link pkg.exoad.app.AppEntry#main(String[])}, your {@code 1}
- * event pool might not be the same as the one found in the default Poprock
- * app. For example, in the Poprock App, event pool {@code 1} represents a
- * singular pipeline for deploying colors. While event pool {@code 2} represents
- * a singular pipeline for passing through commands and other non color related
- * operations to listeners.
+ * measure that goes into categorizing data passing through. Listeners must know the pool
+ * id {@link #registerEventPool(int)} in order to subscribe to it.
+ * </p>
  * <p>
  * <h3>Events can be listened to by anyone</h3>, which can be a good or
- * bad. Especially since there could be credentials being passed around, but
- * look, this app is designed for simply creating gradients not being the next
- * solution to fixing some security pandemic.
+ * bad. Especially since there could be credentials being passed around, but look, this
+ * app is designed for simply creating gradients not being the next solution to fixing
+ * some security pandemic.
+ * </p>
  * <p>
  * <h3>Listeners must know what they want can or cannot exist</h3>,
- * meaning something that wants to listen to anything in the event pool must
- * know that the event they want to subscribe has to exist first.
- * {@link
- * EventPoolService.EventPool#isRegisteredPayloadID(Class)}
+ * meaning something that wants to listen to anything in the event pool must know that the
+ * event they want to subscribe has to exist first.
+ * {@link EventPoolService.EventPool#isRegisteredPayloadID(Class)}
  *
  * <blockquote><pre>
  * EventPool
  * .getPool(1)
  * .attachListener(FooEventPayload.class, () -> {}); // ERROR
  * </pre></blockquote>
- *
+ * </p>
  * <em>The above code snipper throws an error is because
  * {@code FooEventPayload} has not been registered to the EventPool by someone
  * {@link #registerEventPool(int)}
  * <p>
  * <h3>Event Pools are finalized</h3>
- * Once a event pool is registered using {@link #registerEventPool(int)}, there
- * is no going back... <em>Unless you use something quirky reflection
- * manipulation (which highly against!).
- *
+ * Once a event pool is registered using {@link #registerEventPool(int)}, there is no
+ * going back... <em>Unless you use something quirky reflection manipulation (which highly
+ * against!).
+ * </p>
  * @author Jack Meng
  */
 @ServiceClass(requiresArming=false)
 public final class EventPoolService
 {
+	private static final HashMap<Integer,EventPool> OBJECTS=new HashMap<>();
+	
 	private EventPoolService()
 	{
 	}
 	
 	/**
-	 * This is a "marker" interface for all EventPayloads that want to get added
-	 * to any kind of EventPool.
+	 * Returns all of the payload types within an existing event payload.
 	 * <p>
-	 * <strong>Why?</strong> This way EventPool knows that the programmer has
-	 * acknowledged that the class used a payload interface will be used
-	 * properly. (AKA following MVC patterns)
+	 * Has a chance to throw an error because of {@link #getPool(int)}
 	 *
-	 * @author Jack Meng
+	 * @param id identifier of the event pool (int)
+	 *
+	 * @return A list of all of the types in that event pool
 	 */
-	public interface EventPayload
+	public static Optional<Collection<Class<? extends EventPayload>>> inferEventPool(
+		int id
+	)
 	{
+		return Optional
+			.of(
+				getPool(id).payloads
+					.keySet()
+			);
 	}
-	
-	private static final HashMap<Integer,EventPool> OBJECTS=new HashMap<>();
 	
 	/**
 	 * Retrieves a specific EventPool based on an ID.
@@ -105,26 +106,6 @@ public final class EventPoolService
 			);
 		return OBJECTS
 			.get(id);
-	}
-	
-	/**
-	 * Returns all of the payload types within an existing event payload.
-	 * <p>
-	 * Has a chance to throw an error because of {@link #getPool(int)}
-	 *
-	 * @param id identifier of the event pool (int)
-	 *
-	 * @return A list of all of the types in that event pool
-	 */
-	public static Optional<Collection<Class<? extends EventPayload>>> inferEventPool(
-		int id
-	)
-	{
-		return Optional
-			.of(
-				getPool(id).payloads
-					.keySet()
-			);
 	}
 	
 	/**
@@ -153,6 +134,20 @@ public final class EventPoolService
 				new EventPool()
 			);
 		return id;
+	}
+	
+	/**
+	 * This is a "marker" interface for all EventPayloads that want to get added to any
+	 * kind of EventPool.
+	 * <p>
+	 * <strong>Why?</strong> This way EventPool knows that the programmer has
+	 * acknowledged that the class used a payload interface will be used properly. (AKA
+	 * following MVC patterns)
+	 *
+	 * @author Jack Meng
+	 */
+	public interface EventPayload
+	{
 	}
 	
 	public static final class EventPool
@@ -211,8 +206,7 @@ public final class EventPoolService
 		}
 		
 		/**
-		 * Returns the currently cached value of this payload event type (can be
-		 * null).
+		 * Returns the currently cached value of this payload event type (can be null).
 		 *
 		 * @param id idenitifier of the payload
 		 *
@@ -240,9 +234,9 @@ public final class EventPoolService
 		}
 		
 		/**
-		 * Dry runs all of the listeners under an event payload. This should be
-		 * used sparingly as the currently cached value could be unexpected or
-		 * dependent on when it is run.
+		 * Dry runs all of the listeners under an event payload. This should be used
+		 * sparingly as the currently cached value could be unexpected or dependent on
+		 * when it is run.
 		 *
 		 * @param <T> identifier type
 		 * @param id identifier of the payload
